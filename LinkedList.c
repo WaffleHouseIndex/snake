d8d0385f19e78ee0ad33ad453e8cedb7c69ce4a2
whim <barneypwoo@gmail.com>
2022-05-22 21:44:34 +0800

    Full LinkedList implementation initial commit

---
+++ LinkedList.c
@@ -0,0 +1,132 @@
+/*Generic Linked List Standard Implementation*/
+/*Source: https://curtin-ucp.gitbook.io/faq/06-structs */
+
+#include <stdlib.h>
+#include <assert.h>
+#include "LinkedList.h"
+
+LinkedList* createLinkedList()
+{	
+	LinkedList* pLinkedList = (LinkedList*) malloc(sizeof(LinkedList));
+	pLinkedList->pHead = NULL;
+	pLinkedList->pTail = NULL;
+	pLinkedList->iSize = 0;
+
+	return pLinkedList;	
+}
+
+void insertLast(LinkedList* pList, void* pEntry)
+{
+	/* Create a new node */
+	LinkedListNode* pNode = (LinkedListNode*) malloc (sizeof(LinkedListNode));
+	pNode->pData = pEntry;
+	pNode->pNext = NULL;
+		
+	/* If linkedlist is empty */
+	if (pList->pHead == NULL)
+	{
+		assert(pList->pTail == NULL && pList->iSize == 0);		
+		pList->pHead = pNode;
+	}
+	else
+	{
+		assert(pList->pTail && pList->iSize > 0);	
+		pList->pTail->pNext = pNode;
+	}
+	
+	pList->pTail = pNode;
+	(pList->iSize)++;
+	
+}
+
+void* removeLast(LinkedList* pList) 
+{ 
+	LinkedListNode* pCur = pList->pHead;
+	void* pRet = NULL;
+	
+	while (pCur)
+	{		
+		if (pCur->pNext == pList->pTail)
+		{  /* If linkedlist has more than 1 node */
+			
+			/* TODO: set pRet = pTail's pData */
+            pRet = pList->pTail->pData;
+			/* TODO: NULLIFY all fields in pTail */
+            pList->pTail->pData = NULL;
+            pList->pTail->pNext = NULL;
+			/* TODO: free pTail */
+            free(pList->pTail);
+			/* TODO: set pTail = pCur */
+            pList->pTail = pCur;
+			/* TODO: set pTail->pNext = NULL */
+            pList->pTail->pNext = NULL;
+
+			(pList->iSize)--;
+			pCur = NULL;
+			
+			assert(pList->pTail && pList->pHead && pList->iSize > 0);
+			
+		}
+		else if (pList->pHead == pList->pTail)
+		{
+			/* TODO: set pRet = pTail's pData */
+            pRet = pList->pTail->pData;
+			/* TODO: NULLIFY all fields in pTail */
+            pList->pTail->pData=NULL;
+            pList->pTail->pNext=NULL;
+			/* TODO: free pTail */
+            free(pList->pTail);
+			/* TODO: set pTail = pHead = NULL */
+            pList->pTail=NULL;
+            pList->pHead=NULL;
+
+			(pList->iSize)--;
+			pCur = NULL;
+			
+			assert(pList->pTail == NULL && pList->pHead == NULL && pList->iSize == 0);
+			
+		}
+		else
+		{
+			pCur = pCur->pNext;
+		}		
+	}
+	
+	return pRet;
+}
+
+
+
+void printLinkedList(LinkedList* pList, listFunc funcPtr)
+{
+	LinkedListNode* pCur = pList->pHead;
+	
+	while (pCur)
+	{
+		(*funcPtr)(pCur->pData);
+		pCur = pCur->pNext;
+	}
+}
+
+void freeLinkedList(LinkedList* pList, listFunc funcPtr) 
+{
+	LinkedListNode* pCur = pList->pHead;
+	LinkedListNode* pTemp;
+	
+	while (pCur)
+	{
+		/* take pCur->pNext to pTemp */
+		pTemp = pCur->pNext;
+		
+		/* clean pCur */
+		(*funcPtr)(pCur->pData);
+		pCur->pData = NULL;
+		pCur->pNext = NULL;
+		free(pCur);
+		
+		/* set pCur = pTemp */
+		pCur = pTemp;		
+	}	
+	
+	free(pList);
+}
---
+++ LinkedList.h
@@ -0,0 +1,28 @@
+/*Generic Linked List Standard Implementation*/
+/*Source: https://curtin-ucp.gitbook.io/faq/06-structs */
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+typedef void (*listFunc)(void* data);
+
+typedef struct LinkedListNode
+{
+	void* pData;
+	struct LinkedListNode* pNext;
+} LinkedListNode;
+
+typedef struct LinkedList
+{
+	LinkedListNode* pHead;
+	LinkedListNode* pTail;
+	int iSize;	
+} LinkedList;
+
+LinkedList* createLinkedList();
+void insertLast(LinkedList* list, void* entry);
+void* removeLast(LinkedList* list);
+void printLinkedList(LinkedList* list, listFunc funcPtr);
+void freeLinkedList(LinkedList* list, listFunc funcPtr);
+
+#endif
